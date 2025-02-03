4b9d57a14308444ab04ad91f020221522bb1045a
Will Ballantine <156140975+xDefyingGravity@users.noreply.github.com>
2025-02-03 18:20:20 -0500

    Update preload.h

--- preload.h
+++ preload.h
@@ -3,6 +3,6 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-inline duk_ret_t console_log(duk_context *ctx);
-inline duk_ret_t console_error(duk_context *ctx);
+duk_ret_t console_log(duk_context *ctx);
+duk_ret_t console_error(duk_context *ctx);
 void register_console_functions(duk_context *ctx);
